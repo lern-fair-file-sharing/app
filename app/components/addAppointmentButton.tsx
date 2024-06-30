412e901c7be26b2fdb55234e6b3bd114128bae48
ash <zhou@hm.edu>
2024-06-30 23:26:09 +0200

    appointment button styled

--- app/components/addAppointmentButton.tsx
+++ app/components/addAppointmentButton.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
-import { TouchableOpacity, StyleSheet } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { TouchableOpacity, StyleSheet, Image } from 'react-native';
+import Colors from '../utils/Colors';
+
+const calenderAddIcon = require('../../assets/calendar_add.png');
 
 interface AddButtonProps {
-  onPress: () => void;
+    onPress: () => void;
 }
 
 const AddButton: React.FC<AddButtonProps> = ({ onPress }) => {
-  return (
-    <TouchableOpacity style={styles.button} onPress={onPress}>
-      <Icon name="add-circle" size={60} color="#007AFF" />
-    </TouchableOpacity>
-  );
+    return (
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+            <Image 
+                source={calenderAddIcon}
+                style={{ width: 25, height: 25 }}
+                resizeMode='contain'
+            />
+        </TouchableOpacity>
+    );
 };
 
 const styles = StyleSheet.create({
-  button: {
-    position: 'absolute',
-    bottom: 20,
-    right: 20,
-  },
+    button: {
+        position: 'absolute',
+        bottom: 20,
+        right: 20,
+        backgroundColor: Colors.primary,
+        padding: 15,
+        borderRadius: 10,
+    },
 });
 
 export default AddButton;
