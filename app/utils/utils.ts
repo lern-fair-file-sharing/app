e61825c62801a90b9946fef1434f8ee1fcecd03c
nilstesch <nils.tesch03@gmail.com>
2024-07-03 21:13:31 +0200

    chore: hash fileName in fetchImage

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -86,3 +86,12 @@ export const getCurrentDateTimeString = () => {
   
     return `${hours}:${minutes}T${day}-${month}-${year}`;
   };
+
+export function hashString(input: string) {
+    var hash = 0, len = input.length;
+    for (var i = 0; i < len; i++) {
+        hash  = ((hash << 5) - hash) + input.charCodeAt(i);
+        hash |= 0;
+    }
+    return hash;
+}
